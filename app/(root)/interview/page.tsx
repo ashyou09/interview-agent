1fb91c56a823b2dd0f8c7fabc22846f312b06341
Ashutosh Singh <ash.2024@nst.rishihood.edu.in>
2025-04-26 19:30:46 +0530

    again trying

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,21 +1,13 @@
-import Agent from "@/components/Agent";
-import { getCurrentUser } from "@/lib/actions/auth.action";
+import React from 'react'
+import Agent from '@/components/Agent'
 
-const Page = async () => {
-  const user = await getCurrentUser();
-
+const page = () => {
   return (
     <>
-      <h3>Interview generation</h3>
-
-      <Agent
-        userName={user?.name!}
-        userId={user?.id}
-        profileImage={user?.profileURL}
-        type="generate"
-      />
+      <h3>Interview Generation</h3>
+      <Agent userName='You' userId="user1" type='generate' />
     </>
-  );
-};
+  )
+}
 
-export default Page;
+export default page
